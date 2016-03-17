caca68fb5ba5681a9793e4c6769141504b59b6ba
rickie120243 <rickie120243@gmail.com>
2016-03-17 15:07:54 +0800

    add test js

---
+++ sortsyllable/main.js
@@ -0,0 +1,15 @@
+var list = './test.lst';
+var textFolder = './test';
+var fs = require('fs');
+var sortSyllable = require('./sortSyllable.js');
+var bom = String.fromCharCode(0xfeff);
+var fns = fs.readFileSync(list, 'utf8')
+            .replace(bom, '').split(/\r?\n/);
+var totaltext;
+function readFile(fn) {
+  var text = fs.readFileSync(textFolder+ '/' + fn, 'utf8');
+  totaltext += text;
+}
+
+fns.map(readFile);
+console.log(sortSyllable.sortSyllable(totaltext).join('\n'));
